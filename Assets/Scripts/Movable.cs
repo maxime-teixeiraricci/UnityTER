ffc669209b615b2bf6fed9a047c60cfdc5d56dcd
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-02-21 15:20:34 +0100

    Movable modified into interface

--- Assets/Scripts/Movable.cs
+++ Assets/Scripts/Movable.cs
@@ -2,17 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Movable : MoveActions, MoveMethods
+public interface Movable : MoveActions, MoveMethods
 {
-    private double _speed;
-
-    public abstract void move();
-
-    public double getSpeed()
-    {
-        return _speed;
-    }
-
-    public abstract bool isBlocked();
-
 }
