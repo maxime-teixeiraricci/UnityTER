61fca72f83ef126254c17d2bc3ee7a3ba4245744
Maxime Teixeira-Ricci <maxime.teixeira.93@gmail.com>
2018-02-21 15:21:15 +0100

    Ajout des classes de comportement Alive

---
+++ Assets/Scripts/AliveActions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface AliveActions
+{
+    void Kill();
+    void Heal(int value);
+}
---
+++ Assets/Scripts/AliveMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface AliveMethods
+{
+    int GetHealth();
+    void SetHealth(int value);
+    int GetMaxHealth();
+    void SetMaxHealth(int value);
+
+}
