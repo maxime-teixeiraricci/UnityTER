f854f94e7a02caf9280ea139082699187600404c
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-02-21 15:14:50 +0100

    Movable Capacities Added

---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Movable : MoveActions, MoveMethods
+{
+    private double _speed;
+
+    public abstract void move();
+
+    public double getSpeed()
+    {
+        return _speed;
+    }
+
+    public abstract bool isBlocked();
+
+}
