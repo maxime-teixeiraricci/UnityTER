45ecc63c70d00e98f9647e1ccde2ab96494f5fe3
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-02-21 19:45:52 +0100

    Builder, Controllable, Creator and Idler capacities added (miss controllable methods but we need percepts for that)

---
+++ Assets/Scripts/BuilderMethods.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface BuilderMethods {
+
+    GameObject getNextBuildingToBuild();
+    
+    void setNextBuildingToBuild(GameObject nextBuildingToBuild);
+    
+    bool isAbleToBuild(GameObject building);
+    
+    int getIdNextBuildingToRepair();
+    
+    void setIdNextBuildingToRepair(int idNextBuildingToRepair);
+
+}
