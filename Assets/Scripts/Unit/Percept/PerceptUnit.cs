9470b033045dec9c6f9c5aabfe6f3c521dc98614
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-02-23 15:37:29 +0100

    Percepts units and ressource added

---
+++ Assets/Scripts/Unit/Percept/PerceptUnit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerceptUnit : Percept {
+
+    PerceptUnit()
+    {
+        _perceptName = "PERCEPT_UNIT";
+        _value = false;
+        _gameObject = null;
+    }
+
+    override public void update()
+    {
+        Brain brain = GetComponent<Brain>();
+        Sight sight = brain.GetComponent<Sight>();
+        List<GameObject> _listOfUnitColl = new List<GameObject>();
+        foreach(GameObject gO in sight._listOfCollision){
+            if(gO.GetComponent<UnitManager>() != null)
+            {
+                _listOfUnitColl.Add(gO);
+            }
+        }
+        if (_listOfUnitColl.Count > 0)
+        {
+            _value = true;
+            _gameObject = _listOfUnitColl[0];
+        }
+        else
+        {
+            _value = false;
+            _gameObject = null;
+        }
+    }
+
+}
