27e41b4951500123f1c9ddda81a309c329c28dc2
Maxime Teixeira-Ricci <maxime.teixeira.93@gmail.com>
2018-02-23 15:03:46 +0100

    Supprimation de certains classes inutile et Ajoutation d'une nouvelle scene

--- Assets/Scripts/Unit/Manager/Brain.cs
+++ Assets/Scripts/Unit/Manager/Brain.cs
@@ -8,17 +8,10 @@ public class Brain : MonoBehaviour
     public ActionManager[] _actions;
 
 
-    void Awake()
-    {
-        foreach (ActionManager a in _actions)
-        {
-            a._action._target = gameObject;
-        }
-    }
 
     void Update()
     {
-        _actions[0].Do();
+        _actions[0].Do(); // Move
     }
 
 }
