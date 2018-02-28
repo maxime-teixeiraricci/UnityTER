fe67dc5ef846283795e6269023ff77c762a82497
Maxime Teixeira <maxime.teixeira.93@gmail.com>
2018-02-28 15:55:55 +0100

    Test sur les objets scriptables

---
+++ Assets/ItemHeldler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHeldler : MonoBehaviour
+{
+    public Objet _heldObjet;
+
+
+    void Start()
+    {
+        GameObject.Find("Inventory").GetComponent<TestInventory>().objets.Add(_heldObjet);
+    }
+}
---
+++ Assets/Objet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ajouter un objet")]
+public class Objet : ScriptableObject
+{
+    public string _name;
+    public int _value;
+}
