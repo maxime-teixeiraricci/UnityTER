052e08a2a678a99e7c1d50608e132c88d7fb3bc8
Maxime Teixeira <maxime.teixeira.93@gmail.com>
2018-02-28 14:49:24 +0100

    Ajout du comportement des nuages

---
+++ Assets/CloudScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+    public Vector3 _vectMove;
+    public float _speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += _vectMove * Time.deltaTime * _speed;
+        if (transform.position.x <= -30)
+        {
+            transform.position = new Vector3(Random.Range(30f, 45f), 22, Random.Range(-5f, 35f));
+        }
+	}
+}
