ffe2a1e207345824936c8f004d077dd162d8ea70
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-03-03 16:04:04 +0100

    Message MessageManager and new scene

---
+++ Assets/Messages/MessageManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageManager : MonoBehaviour {
+
+    public GameObject sender;
+    public List<Message> _messagesRecus = new List<Message>();
+    public List<Message> _messages = new List<Message>();
+
+    public void sendMessage(Message message)
+    {
+
+        message._receiver.GetComponent<MessageManager>()._messagesRecus.Add(message);
+
+    }
+
+    public void resetMessagesRecus()
+    {
+
+        _messagesRecus = new List<Message>();
+
+    }
+
+    public void createMessage(string titre, string contenu, GameObject receiver)
+    {
+
+        _messages.Add(new Message(sender, titre, contenu, receiver));
+
+    }
+
+    public void resetMessages()
+    {
+
+        _messages = new List<Message>();
+
+    }
+
+}
