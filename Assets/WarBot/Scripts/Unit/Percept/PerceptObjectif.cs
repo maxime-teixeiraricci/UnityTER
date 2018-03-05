990aa4b3681819ca1016e9a42a7128761188ab8e
Mehdi Benlamine <mehdi.ben-lamine@etu.unice.fr>
2018-03-05 16:12:57 +0100

    Ajout percept target et percept objectif

---
+++ Assets/WarBot/Scripts/Unit/Percept/PerceptObjectif.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerceptObjectif : Percept {
+
+    void Start()
+    {
+        _perceptName = "PERCEPT_OBJECTIF";
+        _value = false;
+        _gameObject = null;
+    }
+
+    override public void update()
+    {
+        _value = GetComponent<Brain>().GetComponent<Stats>()._objectif != null;
+    }
+}
---
+++ Assets/WarBot/Scripts/Unit/Percept/PerceptTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerceptTarget : Percept {
+
+    void Start()
+    {
+        _perceptName = "PERCEPT_TARGET";
+        _value = false;
+        _gameObject = null;
+    }
+
+    override public void update()
+    {
+        _value = GetComponent<Brain>().GetComponent<Stats>()._target != null;
+    }
+}
